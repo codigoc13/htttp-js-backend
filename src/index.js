9ec6921538c5b64f98a98346ddf7d51b837f0edb
seraleman <seraleman1204@gmail.com>
2022-08-29 09:03:37 -0500

    Ejecutando promesas en cadena

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 //Haciendo una petición http a hosting
 const jokeUrl = 'https://api.chucknorris.io/jokes/random'
 
-fetch(jokeUrl).then((resp) => {
-  //   console.log(resp)
-  resp.json().then((data) => {
-    console.log(data.id)
-    console.log(data.value)
-    // Desestructurar
+// fetch(jokeUrl).then((resp) => {
+//   //   console.log(resp)
+//   resp.json().then((data) => {
+//     console.log(data.id)
+//     console.log(data.value)
+//     // Desestructurar
+//   })
+// })
+
+fetch(jokeUrl)
+  .then((resp) => resp.json())
+  .then(({ id, value }) => {
+    console.log(id)
+    console.log(value)
   })
-})
