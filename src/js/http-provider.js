b7feae81437b3741ad65aa7f9521eb2e1fc77732
seraleman <seraleman1204@gmail.com>
2022-09-04 18:20:04 -0500

    SE implementa la subida de una imagen

--- src/js/http-provider.js
+++ src/js/http-provider.js
@@ -1,6 +1,10 @@
 const jokeUrl = 'https://api.chucknorris.io/jokes/random'
 const urlUsuarios = 'https://reqres.in/api/users?page=2'
 
+// cloudinary
+const cloudPreset = 'o5vsc7gv'
+const cloudURL = 'https://api.cloudinary.com/v1_1/dk0npadcr/upload'
+
 const obtenerChiste = async () => {
   try {
     const resp = await fetch(jokeUrl)
@@ -19,4 +23,27 @@ const obtenerUsuarios = async () => {
   return usuarios
 }
 
-export { obtenerChiste, obtenerUsuarios }
+// archivoASubir es de tipo File
+const subirImagen = async (archivoASubir) => {
+  const formData = new FormData()
+  formData.append('upload_preset', cloudPreset)
+  formData.append('file', archivoASubir)
+
+  try {
+    const resp = await fetch(cloudURL, {
+      method: 'POST',
+      body: formData,
+    })
+    if (resp.ok) {
+      const cloudResp = await resp.json()
+      // console.log(cloudResp)
+      return cloudResp.secure_url
+    } else {
+      throw await resp.json()
+    }
+  } catch (error) {
+    throw error
+  }
+}
+
+export { obtenerChiste, obtenerUsuarios, subirImagen }
